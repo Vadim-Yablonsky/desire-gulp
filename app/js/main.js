cfb242338b26b018b5f7fb76615c500eb3660520
Your Name <you@example.com>
2021-09-23 22:03:54 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,90 @@
+$(function () {
+
+   $('.header__btn').on('click', function () {
+      $('.rightside-menu').removeClass('rightside-menu--close');
+   });
+   $('.rightside-menu__close').on('click', function () {
+      $('.rightside-menu').addClass('rightside-menu--close');
+   });
+
+
+   $('.header__btn-menu').on('click', function () {
+      $('.menu').toggleClass('menu--open');
+   });
+
+   if ($(window).width() < 651) {
+      $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+   }
+
+
+   $('.top__slider').slick({
+      dots: true,
+      arrows: false,
+      fade: true,
+      autoplay: true,
+   });
+
+   $('.contacts-slider').slick({
+      slidesToScroll: 10,
+      slidesToShow: 10,
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      responsive: [
+         {
+            breakpoint: 1700,
+            settings: {
+               slidesToScroll: 8,
+               slidesToShow: 8,
+            }
+         },
+         {
+            breakpoint: 1511,
+            settings: {
+               slidesToScroll: 6,
+               slidesToShow: 6,
+            }
+         },
+         {
+            breakpoint: 1201,
+            settings: {
+               slidesToScroll: 4,
+               slidesToShow: 4,
+            }
+         },
+         {
+            breakpoint: 841,
+            settings: {
+               slidesToScroll: 3,
+               slidesToShow: 3,
+            }
+         },
+         {
+            breakpoint: 551,
+            settings: {
+               slidesToScroll: 2,
+               slidesToShow: 2,
+            }
+         },
+         {
+            breakpoint: 376,
+            settings: {
+               slidesToScroll: 1,
+               slidesToShow: 1,
+            }
+         }
+      ]
+   });
+
+   $('.article-slider__box').slick({
+      prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+      nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>'
+   });
+
+   var mixer = mixitup('.gallery__inner', {
+      load: {
+         filter: '.living'
+      }
+   });
+
+})
